d3aa24090abb9fe162c04f76f989b3a4255816f6
leehy <leehyun47580@gmail.com>
2022-10-29 18:24:05 +0900

    splash update

--- src/main/java/com/example/howabout/SplashActivity.java
+++ src/main/java/com/example/howabout/SplashActivity.java
@@ -8,10 +8,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 
 import com.example.howabout.API.RetrofitClient;
-import com.example.howabout.Vo.LoginDTO;
 import com.example.howabout.functions.HowAboutThere;
 
 import java.util.Map;
@@ -34,18 +32,21 @@ public class SplashActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("USER", Activity.MODE_PRIVATE);
         boolean CODE_autoLogin = sharedPreferences.getBoolean("auto", false);
-
+        Log.e("leehj", CODE_autoLogin + "");
         if (CODE_autoLogin) { //자동로그인을 선택한 경우
             String token = sharedPreferences.getString("token", null);
             if (token != null) { //LOGIN 파일에 token 값이 있으면, 자동로그인 성공
                 Log.i("leehj", "splash/ 자동로그인 선택 token이 " + token);
-                reToken("Bearer "+token); //토큰 재발급
+                reToken("Bearer " + token); //토큰 재발급
                 splash(1);
             } else { //token이 null이면
                 Log.i("leehj", "splash/ 자동로그인 선택 token이 null");
                 splash(1);
             }
         } else { //자동 로그인을 선택하지 않을 경우
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.commit();
             Log.i("leehj", "splash/ 자동로그인 선택 안함");
             splash(1);
         }
@@ -62,19 +63,22 @@ public class SplashActivity extends AppCompatActivity {
         }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
     }
 
-    public void reToken(String token){
+    public void reToken(String token) {
         Call<Map<String, String>> login = RetrofitClient.getApiService().autoLogin(token);
         login.enqueue(new Callback<Map<String, String>>() {
             @Override
             public void onResponse(Call<Map<String, String>> call, Response<Map<String, String>> response) {
                 Map<String, String> result = response.body();
-                String response_token = result.get("jwt");
-                Log.e("leehj", "재발급 토큰 : "+response_token);
-                sharedPreferences = getSharedPreferences("USER", Activity.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("token", response_token);
-                editor.commit();
-                Log.i("leehj", "공유 프레퍼런스 저장 토큰: "+sharedPreferences.getString("token", null)); //@@
+                if (result != null) {
+                    String response_token = result.get("jwt");
+                    Log.e("leehj", "재발급 토큰 : " + response_token);
+                    sharedPreferences = getSharedPreferences("USER", Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("token", response_token);
+                    editor.commit();
+                    Log.i("leehj", "공유 프레퍼런스 저장 토큰: " + sharedPreferences.getString("token", null)); //@@
+
+                }
             }
 
             @Override
